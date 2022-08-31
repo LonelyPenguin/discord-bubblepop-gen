548bda13c801e0be6efab433c3584e947f371562
LonelyPenguin <86080520+LonelyPenguin@users.noreply.github.com>
2022-08-30 21:47:19 -0700

    basic: parse data and display result

--- logic.py
+++ logic.py
@@ -16,11 +16,32 @@ def gen_page():
         myform = request.form
 
         if myform.get("editor") == "grid" or myform.get("editors_select") == "grid":
-            print("using grid editor")
+            #use grid editor
             return grid_input(myform)
 
         elif myform.get("editor") == "basic" or myform.get("editors_select") == "basic":
-            print("using basic editor")
+            # use basic editor
+
+            try:
+                input = myform.get("freeform_entry")
+                in_rows = input.splitlines()
+
+                #check input instead of each row to be consistent with splitting behaviour for whole input
+                if "," in input:
+                    rows_and_columns = [row.split(",") for row in in_rows]
+                else:
+                    rows_and_columns = [row.split(" ") for row in in_rows]
+
+                spoilered_rows = []
+                for row in rows_and_columns:
+                    spoilered_rows.append("||" + "||||".join(row) + "||")
+
+                output = "\n".join(spoilered_rows)
+
+                mycontext["final_output"] = output
+
+            except AttributeError:
+                pass
 
     return render_template("gen_page.html", context=mycontext)
 
@@ -37,6 +58,7 @@ def grid_input(form):
     mycontext["set_rows"] = 8
     mycontext["set_cols"] = 8
     mycontext["prefill"] = ""
+    mycontext["final_output"] = "grid editor"
 
     # override defaults, if dimensions provided
     if num_rows:
